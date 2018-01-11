7d223ebca7735859ec1945103fc87d6fa9ca6b54
DESKTOP-1GO7OG8\Tsend-Ayush <tsendayush0412@gmail.com>
2018-01-11 09:51:46 +0800

    blabla

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ function sendTextMessage(recipientId, messageText){
 function callSendAPI(messageData){
 	request({
 		uri: "https://graph.facebook.com/v2.6/me/messages",
-		qs: { "access_token": access },
+		qs: { access_token: access },
 		method : "POST",
 		json : messageData;
 	},
