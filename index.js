bf2e7caf24a59fcb65c5a2b3e0bb04706eb1766e
DESKTOP-1GO7OG8\Tsend-Ayush <tsendayush0412@gmail.com>
2018-01-11 09:42:28 +0800

    next setup code!

--- index.js
+++ index.js
@@ -66,7 +66,76 @@ app.post('/webhook', (req, res) => {
 });
 
 function receivedMessage(event){
-	console.log('message data: ',event.message);
+	
+	var senderID = event.serder.id;
+	var recipientID = event.recipient.id;
+	var timeOfMessage = event.timestamp;
+	var message = event.message;
+
+	console.log("received message for user %d and page %d at %d with message: ",
+		senderID,recipientID,timeOfMessage);
+
+	console.log(JSON.stringify(message));
+
+	var messageId = message.mid;
+	var messageText = message.text;
+	var messageAttachments = message.attachments;
+
+	if(messageText){
+
+		switch(messageText){
+
+			case "generic":
+			sendGenericMessage(senderID);
+			break;
+			default:
+
+			sendTextMessage(senderID,messageText);
+		}
+
+	}else if(messageAttachments){
+		sendTextMessage(senderID,"Message with attachments recieved");
+	}
+}
+
+function sendGenericMessage(recipientId, messageText){
+	
+}
+
+function sendTextMessage(recipientId, messageText){
+	var messageData = {
+		recipient:{
+			id: recipientId;
+		},
+		message:{
+			text:messageText;
+		}
+	};
+
+	callSendAPI(messageData);
+}
+
+function callSendAPI(messageData){
+	request({
+		uri: "https://graph.facebook.com/v2.6/me/messages",
+		qs: { "access_token": access },
+		method : "POST",
+		json : messageData;
+	},
+	function(error,response,body){
+
+		if(!error && response.statusCode == 200){
+			var recipientId = body.recipient_id;
+			var messageId = body.message_id;
+
+			console.log('Successfully sent generic message with id %s to recipient %s',
+				messageId, recipientId);
+		}else{
+			console.error("unable to send message.")
+			console.error(response);
+			console.error(error);
+		}
+	});
 }
 
 app.listen(app.get('port'), function(){
